4fe9568936cbcf011fb8085b2531961db085297c
Greg Thompson II <gregthompsonnms@gmail.com>
2022-03-06 00:35:23 -0600

    addtional questions + bug fix

--- JavaScript and HTML projects/QuizApp/quiz.js
+++ JavaScript and HTML projects/QuizApp/quiz.js
@@ -40,6 +40,7 @@ function showQuestion(question) {
 }
 
 function resetState() {
+    clearStatusClass(document.body)
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild(answerButtonsElement.firstChild)
@@ -84,5 +85,71 @@ const questions = [
             {text: 'Private Home Page', correct: false},
             {text: 'Personal Home Page', correct: false}
         ]
+    },
+    {
+        question: 'How do you write "Hello World" in PHP?',
+        answers: [
+            {text: '"Hello World";', correct: false},
+            {text: 'Document.write("Hello World")', correct: false},
+            {text: 'echo "Hello World"', correct: true}
+        ],
+    },
+    {
+        question: 'All variables in PHP start with what symbol?',
+        answers: [
+            {text: '$', correct: true},
+            {text: '@', correct: false},
+            {text: '%', correct: false},
+            {text: '&', correct: false}
+        ],
+    },
+    {
+        question: 'What is the correct way to add 1 to the $count variable?',
+        answers: [
+            {text: '$count++', correct: false},
+            {text: '$count =+ 1', correct: true},
+            {text: '++count', correct: false},
+            {text: 'count++', correct: false}
+        ],
+    },
+    {
+        question: 'How do you get information from a form that is submitted using the "post" method?',
+        answers: [
+            {text: 'Request.Form', correct: false},
+            {text: '$_GET[];', correct: false},
+            {text: '$_POST[];', correct: true},
+            {text: 'Request.QueryString;', correct: false}
+        ],
+    },
+    {
+        question: 'When using the post method, variables are displayed in the URL:',
+        answers: [
+            {text: 'True', correct: false},
+            {text: 'False', correct: true}
+        ],
+    },
+    {
+        question: 'In PHP, you can use both single quotes and double quotes for stings:',
+        answers: [
+            {text: 'True', correct: true},
+            {text: 'False', correct: false}
+        ],
+    },
+    {
+        question: 'What is the correct way to create a function in PHP?',
+        answers: [
+            {text: 'new_function myFunction()', correct: false},
+            {text: 'create myFuntion()', correct: false},
+            {text: 'function myFunction()', correct: true}
+        ],
+    },
+    {
+        question: 'What is the correct way to connect to a MySQL database?',
+        answers: [
+            {text: 'mysql_connect("localhost");', correct: true},
+            {text: 'mysql_query("localhost");', correct: false},
+            {text: 'connect_mysql("localhost"); ', correct: false},
+            {text: 'mysql_open("localhost");', correct: false}
+        ],
     }
 ]
